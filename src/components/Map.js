059dfd8b453d4c93c331949076aaafb958ab8170
Utkarsh Agrawal <agrawal.utkarsh8@gmail.com>
2021-02-24 20:45:19 +0530

    load restaurant markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import H from "@here/maps-api-for-javascript";
+import firebase from "firebase/app"
 
 
 export default class HereMap extends Component{
@@ -10,6 +11,8 @@ export default class HereMap extends Component{
 
         this.ref = React.createRef();
         this.map = null;
+        this.group = null;
+        this.ui = null;
 
         this.state = {
             apikey : props.apikey,
@@ -19,12 +22,14 @@ export default class HereMap extends Component{
             },
             zoom: props.zoom,
             theme: props.theme,
+            restaurants : [],
         }
 
         this.locationListener = this.locationListener.bind(this)
+        this.restaurantListener = this.restaurantListener.bind(this)
+        this.addMarkerToGroup = this.addMarkerToGroup.bind(this)
     }
 
-    
 
     componentDidMount()
     {
@@ -50,16 +55,30 @@ export default class HereMap extends Component{
     
               window.addEventListener('resize', () => this.map.getViewPort().resize());
               var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
-              var ui = H.ui.UI.createDefault(this.map, layer);
+              this.ui = H.ui.UI.createDefault(this.map, layer);
+
+              this.group = new H.map.Group();
+              this.map.addObject(this.group);
+      
+                this.group.addEventListener('tap', (evt) => {
+                var bubble =  new H.ui.InfoBubble(evt.target.getGeometry(), {
+                    content: evt.target.getData()
+                });
+
+                this.ui.addBubble(bubble);
+                }, false);
+        
         }
         
 
           this.locationListener();
-
+          this.restaurantListener();
     }
 
+
+
       locationListener()
-    {
+      {
         if(navigator.geolocation) 
         {
             navigator.geolocation.getCurrentPosition(position => {
@@ -84,6 +103,36 @@ export default class HereMap extends Component{
 
     }
 
+
+    restaurantListener(){
+
+        var restaurants = [];
+        var db = firebase.firestore();
+
+        db.collection("restaurant").get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                restaurants.push(doc.data());
+            });
+                restaurants.forEach((doc) => {
+                this.addMarkerToGroup({lat: doc.coords.x_, lng: doc.coords.N_},
+                    '<div><h1>' + doc.name + '</h1>' +
+                    '</div><div >Rating : ' + String(doc.ratings.overall_rating) + '</div>')
+            })
+        });
+
+        this.setState({
+            restaurants : restaurants,
+        })
+    }
+
+
+    addMarkerToGroup(coordinate, html) {
+        var marker = new H.map.Marker(coordinate);
+        marker.setData(html);
+        this.group.addObject(marker);
+      }
+      
 
 
 
